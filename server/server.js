4c331cda7c54b1082d5bb11dbbd947ee9cc9e555
Daniel Norman <danielnorman@Daniels-MacBook-Pro.local>
2018-03-13 22:39:26 +0000

    Set Access-Control-Allow-Origin

--- server/server.js
+++ server/server.js
@@ -5,12 +5,28 @@ var express = require('express'),
   
 app.use(bodyParser.json());
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  next();
+});
+
 var routes = require('./routes/birthdayRoutes'); //importing route
 routes(app); //register the route
 
 app.listen(port);
 
 console.log('Birthdays API server started on: ' + port);
+
 
 app.use(function(req, res) {
     res.status(404).send({url: req.originalUrl + ' not found'})
