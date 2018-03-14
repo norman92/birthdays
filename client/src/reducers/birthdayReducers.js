56e3a6d447bc0a59882c99f9e46603aa3e53f807
Daniel Norman <danielnorman@Daniels-MacBook-Pro.local>
2018-03-14 00:08:49 +0000

    Update birthday reducer to handle add birthday

--- client/src/reducers/birthdayReducers.js
+++ client/src/reducers/birthdayReducers.js
@@ -1,6 +1,8 @@
 import {
   REQUEST_BIRTHDAYS,
-  RECEIVE_BIRTHDAYS
+  RECEIVE_BIRTHDAYS,
+  POST_BIRTHDAY,
+  BIRTHDAY_SAVED
 } from './../actions/birthdayActions'
  
 function birthdays(
@@ -15,15 +17,33 @@ function birthdays(
     case REQUEST_BIRTHDAYS:
       return Object.assign({}, state, {
         isFetching: true,
-        didInvalidate: false
+        birthdaySaved: false,   
+        birthdaySaving: false             
       })
     case RECEIVE_BIRTHDAYS:
       return Object.assign({}, state, {
         isFetching: false,
-        didInvalidate: false,
         birthdays: action.birthdays,
-        lastUpdated: action.receivedAt
+        lastUpdated: action.receivedAt,
+        birthdaySaved: false,
+        birthdaySaving: false
       })
+    case POST_BIRTHDAY:
+      return Object.assign({}, state, {
+        isFetching: false,
+        birthdays: state.birthdays,
+        lastUpdated: action.receivedAt,
+        birthdaySaved: false,
+        birthdaySaving: true
+      })
+    case BIRTHDAY_SAVED:
+      return Object.assign({}, state, {
+        isFetching: false,
+        birthdays: [...state.birthdays, action.birthday],
+        lastUpdated: action.receivedAt,
+        birthdaySaved: true,
+        birthdaySaving: false
+      })      
     default:
       return state
   }
